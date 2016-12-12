855d24ea14d57df85efe25b8a7ad0e4354b7816c
sergey30 <sergeykonev@icloud.com>
2016-12-12 14:29:38 +0700

    Revert "рабочая версия"
    This reverts commit 08fe40ce45bbba66f38ca5a73be6187d08caf85a.

--- client_test.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-socket_client = socket.socket()
-socket_client.connect(("127.0.0.1", 9090))
-#data = input("Write number:")
-socket_client.send(str.encode("hi"))
-data = socket_client.recv(1024)
-socket_client.close()
-print("Received:", bytes.decode(data))
