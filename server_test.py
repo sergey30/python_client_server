fc7a4ef2bbc16398a8e79787a0efa557a6172cfd
sergey30 <sergeykonev@icloud.com>
2016-12-12 14:31:28 +0700

    Revert "Revert "рабочая версия""
    This reverts commit 855d24ea14d57df85efe25b8a7ad0e4354b7816c.

---
+++ server_test.py
@@ -0,0 +1,18 @@
+import socket
+
+socket_server = socket.socket()
+socket_server.bind(("", 9090))
+socket_server.listen(10)
+
+while True:
+    question = input("Do you want to out? y/n: ")
+    if question == "y":
+        break
+    socket_client, address_client = socket_server.accept()
+    print("Connected by:", address_client)
+    while True:
+        data = socket_client.recv(1024)
+        if not data:
+            break
+        socket_client.send(data.upper())
+    socket_client.close()
