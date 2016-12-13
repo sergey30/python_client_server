b252e377b973575e4c486ee122ea2a9a30b293bf
sergey30 <sergeykonev@icloud.com>
2016-12-13 16:22:33 +0700

    работает в два потока, заканчиваются порты

--- client_test.py
+++ client_test.py
@@ -1,8 +1,10 @@
 import socket
 
-socket_client = socket.socket()
-socket_client.connect(("127.0.0.1", 9090))
-socket_client.send("hi 1".encode())
-data = socket_client.recv(1024)
-socket_client.close()
-print("Received:", data.decode())
+
+while True:
+    socket_client = socket.socket()
+    socket_client.connect(("127.0.0.1", 9090))
+    socket_client.send("hi 1".encode())
+    data = socket_client.recv(1024)
+    socket_client.close()
+    print("Received:", data.decode())
--- server_test.py
+++ server_test.py
@@ -2,12 +2,12 @@ import socket
 
 socket_server = socket.socket()
 socket_server.bind(("", 9090))
-socket_server.listen(1)
+socket_server.listen(10)
 
 while True:
-    question = input("Do you want to out? y/n: ")
-    if question == "y":
-        break
+    #question = input("Do you want to out? y/n: ")
+    #if question == "y":
+    #    break
     socket_client, address_client = socket_server.accept()
     print("Connected by:", address_client)
     while True:
