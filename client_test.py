f0f4f4c9bb53799d7eb778f7801d7d4a9938d4de
sergey30 <sergeykonev@icloud.com>
2016-12-13 11:44:16 +0700

    добавил  encode/decode

--- client_test.py
+++ client_test.py
@@ -2,8 +2,7 @@ import socket
 
 socket_client = socket.socket()
 socket_client.connect(("127.0.0.1", 9090))
-#data = input("Write number:")
-socket_client.send(str.encode("hi"))
+socket_client.send("hi".encode())
 data = socket_client.recv(1024)
 socket_client.close()
-print("Received:", bytes.decode(data))
+print("Received:", data.decode())
