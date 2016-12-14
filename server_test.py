723861c74aecd9bcf010b1ca71ab7d7ca844369d
sergey30 <sergeykonev@icloud.com>
2016-12-14 16:43:06 +0700

    Рабочая версия
    Клиент читает из файла, передает серверу, сервер записывает в файл,
    переводит в верх регистр и возвращает клиенту

--- server_test.py
+++ server_test.py
@@ -11,8 +11,10 @@ while True:
     socket_client, address_client = socket_server.accept()
     print("Connected by:", address_client)
     while True:
-        data = socket_client.recv(1024)
-        if not data:
+        data_from_client = socket_client.recv(1024)
+        if not data_from_client:
             break
-        socket_client.send(data.upper())
+        data = open("server_test.txt", "a")
+        data = data.write(data_from_client.decode())
+        socket_client.send(data_from_client.upper())
     socket_client.close()
