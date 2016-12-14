88f134d771edb3040e6f846bac7387c631d33f24
sergey30 <sergeykonev@icloud.com>
2016-12-14 12:35:52 +0700

    Рабочая версия
    Два клиента передают сообщение серверу и получают обратно это сообщение
    буквами в верхнем регистре.

--- client_test_1.py
+++ client_test_1.py
@@ -4,7 +4,7 @@ import socket
 while True:
     socket_client = socket.socket()
     socket_client.connect(("127.0.0.1", 9090))
-    socket_client.send("hi 1".encode())
+    socket_client.send("I am client 1".encode())
     data = socket_client.recv(1024)
     socket_client.close()
     print("Received:", data.decode())
