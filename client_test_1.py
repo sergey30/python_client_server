c8d04583716787d58ab842fb9bf4a6990ca8157a
sergey30 <sergeykonev@icloud.com>
2016-12-14 16:28:06 +0700

    читает из файла и выводит в терминал

--- client_test_1.py
+++ client_test_1.py
@@ -1,10 +1,10 @@
 import socket
 
-
-while True:
-    socket_client = socket.socket()
-    socket_client.connect(("127.0.0.1", 9090))
-    socket_client.send("I am client 1".encode())
-    data = socket_client.recv(1024)
-    socket_client.close()
-    print("Received:", data.decode())
+socket_client = socket.socket()
+socket_client.connect(("127.0.0.1", 9090))
+data = open("client_test_1.txt", "rb") # читаю из файла в байтах
+data = data.read()
+socket_client.send(data) # не использую encode() т.к. прочитано из файла в байтах
+data = socket_client.recv(1024)
+socket_client.close()
+print("Received:", data.decode())
--- client_test_2.py
+++ client_test_2.py
@@ -1,10 +1,10 @@
 import socket
 
-
-while True:
-    socket_client = socket.socket()
-    socket_client.connect(("127.0.0.1", 9090))
-    socket_client.send("I am client 2".encode())
-    data = socket_client.recv(1024)
-    socket_client.close()
-    print("Received:", data.decode())
+socket_client = socket.socket()
+socket_client.connect(("127.0.0.1", 9090))
+data = open("client_test_2.txt", "rb")
+data = data.read()
+socket_client.send(data)
+data = socket_client.recv(1024)
+socket_client.close()
+print("Received:", data.decode())
