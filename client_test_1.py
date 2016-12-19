424bfb5752a8d2bfa95f1e24927cb1f89b6852b1
sergey30 <sergeykonev@icloud.com>
2016-12-19 14:58:22 +0700

    Revert "Клиенты отправляют данные частями, сервер сам не выкл"
    This reverts commit 0b65cc4de9226c5fa90c10061b2c3025ad1b7c80.

--- client_test_1.py
+++ client_test_1.py
@@ -1,13 +1,10 @@
 import socket
-import time
 
-data = open("client_test_1.txt", "rb")
-for line in data:
-    socket_client = socket.socket()
-    socket_client.connect(("127.0.0.1", 9090))
-    socket_client.send(line)
-    time.sleep(1)
-    socket_client.close()
+socket_client = socket.socket()
+socket_client.connect(("127.0.0.1", 9090))
+data = open("client_test_1.json", "rb")
+socket_client.send(data.read())
 data.close()
-
+#data = socket_client.recv(1024)
+socket_client.close()
 print("Ready")
--- server_test.py
+++ server_test.py
@@ -5,10 +5,17 @@ socket_server.bind(("", 9090))
 socket_server.listen(10)
 
 while True:
+    question = input("Do you want to out? y/n: ")
+    if question == "y":
+        break
     socket_client, address_client = socket_server.accept()
     print("Connected by:", address_client)
-    data_from_client = socket_client.recv(1024)
-    data = open("server_test.txt", "a")
-    data.write(data_from_client.decode())
-    data.close()
+    while True:
+        data_from_client = socket_client.recv(1024)
+        if not data_from_client:
+            break
+        data = open("server_test.txt", "a")
+        data.write(data_from_client.decode())
+        data.close()
+        #socket_client.send(data_from_client.upper())
     socket_client.close()
