285b7a5a106bbee6f103979a7a9027677451962e
sergey30 <sergeykonev@icloud.com>
2016-12-20 11:58:06 +0700

    Выделил прием и запись данных  в функцию

--- server_test.py
+++ server_test.py
@@ -4,17 +4,21 @@ socket_server = socket.socket()
 socket_server.bind(("", 9090))
 socket_server.listen(10)
 
+def record_data():
+    data_from_client = socket_client.recv(1024)
+    if not data_from_client:
+        return
+    data = open("server_test.txt", "a")
+    data.write(data_from_client.decode())
+    data.close()
+    record_data()
+
 while True:
     question = input("Do you want to out? y/n: ")
     if question == "y":
         break
     socket_client, address_client = socket_server.accept()
     print("Connected by:", address_client)
-    while True:
-        data_from_client = socket_client.recv(1024)
-        if not data_from_client:
-            break
-        data = open("server_test.txt", "a")
-        data.write(data_from_client.decode())
-        data.close()
-    socket_client.close()
+    record_data()
+
+socket_client.close()
