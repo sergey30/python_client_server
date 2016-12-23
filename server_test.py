46519fd47088f63b26c27323b8434d311ec3d04d
sergey30 <sergeykonev@icloud.com>
2016-12-23 15:19:24 +0700

    Переделал структуру на сервере, начал делать отдельный процесс для записи sql

--- server_test.py
+++ server_test.py
@@ -1,21 +1,31 @@
 import socket
 import umsgpack
+import multiprocessing
 
 socket_server = socket.socket()
 socket_server.bind(("", 9090))
 socket_server.listen(10)
 
-def record_data():
+def record_json():
     while True:
+        socket_client, address_client = socket_server.accept()
+        print("Connected by:", address_client)
         data_from_client = socket_client.recv(1024)
         if not data_from_client:
-            return
+            continue
         data = open("server_test.json", "a")
         data.write(umsgpack.unpackb(data_from_client))
         data.close()
+        socket_client.close()
 
-while True:
-    socket_client, address_client = socket_server.accept()
-    print("Connected by:", address_client)
-    record_data()
-    socket_client.close()
+def record_sql():
+    data = open("server_test.json")
+    data_json = data.read()
+    data.close()
+    data_sql = open("server_test.txt", "a")
+    data_sql.write(data_json)
+    data_sql.close()
+
+#record = multiprocessing.Process(target = record_sql, args = ())
+
+record_json()
