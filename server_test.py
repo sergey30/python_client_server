56d5943530ee8d226df1aa6b3bb2d41936316e4e
sergey30 <sergeykonev30@gmail.com>
2016-12-30 09:25:39 +0700

    Рабочая версия(дополнил)

--- server_test.py
+++ server_test.py
@@ -8,14 +8,17 @@ socket_server.listen(10)
 
 def record_json():
     while True:
-        socket_client, address_client = socket_server.accept()
-        data_from_client = socket_client.recv(1024)
-        if not data_from_client:
-            continue
-        data = open("server_test.json", "a")
-        data.write(umsgpack.unpackb(data_from_client))
-        data.close()
-        socket_client.close()
+        try:
+            socket_client, address_client = socket_server.accept()
+            data_from_client = socket_client.recv(1024)
+            if not data_from_client:
+                continue
+            data = open("server_test.json", "a")
+            data.write(umsgpack.unpackb(data_from_client))
+            data.close()
+            socket_client.close()
+        except TypeError:
+            print("Data must be in json format")
 
 def record_sql():
     counter = 0
@@ -25,7 +28,6 @@ def record_sql():
         result = len(list(data_json)) > len(list(data_sql))
         data_json.close()
         data_sql.close()
-
         if result:
             data_json = open("server_test.json")
             data_sql = open("server_test.txt", "a")
