1d50ef56daa34c842fc9fafc29f07eb34c9ef234
sergey30 <1>
2017-01-17 17:52:42 +0700

    Выполненное задание, но данные в базе без id
    Если сделать поле "id serial primary key" в итоговой таблице, то
    нумерация получается не корректная (1, 2, 45, 64, и тд.). Без id данные
    нормально вносятся.

--- server.py
+++ server.py
@@ -9,22 +9,25 @@ socket_server.listen(10)
 
 def record_json():
     while True:
-            socket_client, address_client = socket_server.accept()
-            data_from_client = socket_client.recv(1024)
-            if not data_from_client:
-                continue
-            with open("server.json", "a") as data:
-                data.write(umsgpack.unpackb(data_from_client))
-            socket_client.close()
+        socket_client, address_client = socket_server.accept()
+        data_from_client = socket_client.recv(1024)
+        if not data_from_client:
+            continue
+        with open("server.json", "a") as data:
+            data.write(umsgpack.unpackb(data_from_client))
+        socket_client.close()
 
 def record_sql():
     connect_db = psycopg2.connect(database="test_db", user="postgres", password="13", host="127.0.0.1", port="5432")
     cursor_db = connect_db.cursor()
 
-    cursor_db.execute("CREATE TABLE clients (id SERIAL PRIMARY KEY, data TEXT);")
+    cursor_db.execute("CREATE TABLE clients ( data TEXT UNIQUE);")
 
     while True:
-        cursor_db.execute("COPY clients(data) FROM '/Users/s/Library/Mobile Documents/com~apple~CloudDocs/programming/python_client_server/server.json'")
+        cursor_db.execute("CREATE TABLE temp_table (data TEXT);")
+        cursor_db.execute("COPY temp_table(data) FROM '/Users/s/Library/Mobile Documents/com~apple~CloudDocs/programming/python_client_server/server.json';")
+        cursor_db.execute("INSERT INTO clients (data) SELECT data FROM temp_table ON CONFLICT DO NOTHING;")
+        cursor_db.execute("DROP TABLE temp_table;")
         connect_db.commit()
 
     connect_db.close()
