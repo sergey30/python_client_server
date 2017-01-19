80d0ff7151aeacb099bb07018f955a21a1195f35
sergey30 <1>
2017-01-19 16:07:51 +0700

    Рабочая версия

--- client2.py
+++ client2.py
@@ -1,10 +1,13 @@
 import socket
 import umsgpack
 
+# Открывается файл с данными которые передадутся серверу.
 data = open("client2.json")
+# В цикле данные передаются порциями по одной строке.
 for i in data:
     socket_client = socket.socket()
     socket_client.connect(("127.0.0.1", 9090))
+    # Перед отправкой данные упаковываются в MSGPACK.
     socket_client.send(umsgpack.packb(i))
     socket_client.close()
 data.close()
